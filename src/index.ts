0f47eb30e843d9b70535e23346f07ea56d2f67d6
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-04-18 18:47:08 -0400

    Remove cors from express and unused route

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,13 @@
 import express from 'express';
 import http from 'http';
-import cors from 'cors';
 import { Server } from 'socket.io';
 
 const app = express();
-
-app.use(cors());
-
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: 'http://localhost.localdomain:3000',
   },
-});
-
-app.get('/', (_req, res) => {
-  res.send('<h1>hello world</h1>');
 });
 
 io.on('connection', (socket) => {
