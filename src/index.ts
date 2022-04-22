697b856fdef165fcbc9a0701b22d4be425fd483f
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-04-21 22:41:58 -0400

    Implement ChatMessage parsing and type checking

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
+import { Record as RtRecord, String as RtString } from 'runtypes';
 
 const app = express();
 const server = http.createServer(app);
@@ -10,13 +11,22 @@ const io = new Server(server, {
   },
 });
 
+const ChatMessageType = RtRecord({
+  timestamp: RtString,
+  message: RtString,
+});
+
 io.on('connection', (socket) => {
   console.log('a user connected: ', socket.id);
 
-  socket.on('message', (message) => {
-    console.log('user [', socket.id, '] sent message: ', message);
-
-    socket.broadcast.emit('message', message);
+  socket.on('message', (payload) => {
+    try {
+      const { timestamp, message } = ChatMessageType.check(JSON.parse(payload));
+      console.log(`user [${socket.id}] sent message [${timestamp}]:  ${message}`);
+      socket.broadcast.emit('message', payload);
+    } catch (error: unknown) {
+      console.error(error);
+    }
   });
 
   socket.on('disconnect', () => {
