a1ff8f7809e48f60683b6139dd651da1a0be5235
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-05-13 23:00:46 -0400

    Implement config and initialization for sequelize

---
+++ src/utils/db.ts
@@ -0,0 +1,30 @@
+import { Sequelize } from 'sequelize';
+import { NODE_ENV, DATABASE_URL } from '../config';
+
+let options = {};
+
+if (NODE_ENV === 'production') {
+  // Recommended for production database to use SSL
+  options = {
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  };
+}
+
+export const sequelize = new Sequelize(DATABASE_URL, options);
+
+export const connectToDatabase = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connected to database');
+  } catch {
+    console.log('Failed to connect to database');
+  }
+};
+
+export default sequelize;
