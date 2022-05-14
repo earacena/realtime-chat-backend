a3de84f1c849ebdbe26222d3abad988f604bcaa6
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-05-13 22:56:08 -0400

    Add env variable validation

---
+++ src/config/index.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+import { String as RtString } from 'runtypes';
+
+const determineDatabaseUrl = (nodeEnv: string): string => {
+  switch (nodeEnv) {
+    case 'development':
+      return RtString.check(process.env['DEV_DATABASE_URL']);
+    case 'testing':
+      return RtString.check(process.env['TEST_DATABASE_URL']);
+    default:
+      return '';
+  }
+};
+
+export const NODE_ENV = RtString.check(process.env['NODE_ENV']);
+export const DATABASE_URL = determineDatabaseUrl(NODE_ENV);
+export const PORT = Number(RtString.check(process.env['PORT']));
