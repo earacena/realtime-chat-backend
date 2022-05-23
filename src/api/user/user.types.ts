8ad4b6b52d5910dfba45431385a0e2f9bf4b53e4
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-05-23 19:02:13 -0400

    Add type definition for User type

---
+++ src/api/user/user.types.ts
@@ -0,0 +1,31 @@
+import {
+  Number as RtNumber,
+  String as RtString,
+  Record as RtRecord,
+  Static as RtStatic,
+  Array as RtArray,
+  Union as RtUnion,
+  InstanceOf as RtInstanceOf,
+  ValidationError,
+} from 'runtypes';
+
+export const User = RtRecord({
+  id: RtNumber,
+  name: RtString,
+  username: RtString,
+  passwordHash: RtString,
+  dateRegistered: RtUnion(
+    RtInstanceOf(Date),
+    RtString.withConstraint((x: string) => (x || x !== null || typeof x === 'string' || !Number.isNaN(Date.parse(x)))),
+  ),
+});
+
+export const UserPostRequest = RtRecord({
+  name: RtString,
+  username: RtString,
+  password: RtString,
+});
+
+export const UserArray = RtArray(User);
+export type Users = RtStatic<typeof UserArray>;
+export const RtValidationError = RtInstanceOf(ValidationError);
