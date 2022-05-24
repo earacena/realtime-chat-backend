e6465995cfc361fb761f8384fb246e29ebde4b41
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-05-23 22:21:21 -0400

    Add type validation to create user controller

--- src/api/user/user.controllers.ts
+++ src/api/user/user.controllers.ts
@@ -1,16 +1,19 @@
 import { Request, Response } from 'express';
 import bcrypt from 'bcrypt';
 import User from './user.model';
+import { createUserRequest, User as UserType } from './user.types';
 
 const createUserController = async (req: Request, res: Response) => {
   try {
-    const { name, username, password } = req.body;
+    const { name, username, password } = createUserRequest.check(req.body);
     const passwordHash = await bcrypt.hash(password, 10);
-    const newUser = await User.create({
-      name,
-      username,
-      passwordHash,
-    });
+    const newUser = UserType.check(
+      await User.create({
+        name,
+        username,
+        passwordHash,
+      }),
+    );
 
     res.status(201).json(newUser);
   } catch (error: unknown) {
