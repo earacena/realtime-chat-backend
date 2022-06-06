c7d4e36bb4ac173820277236fbc6d2097663ef83
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-06-06 19:53:48 -0400

    Add controller to request router

--- src/api/request/request.routes.ts
+++ src/api/request/request.routes.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import requestControllers from './request.controllers';
 
-const { createRequestController } = requestControllers;
+const { createRequestController, getPendingRequestsForUserId } = requestControllers;
 
 const requestRouter = express.Router();
 
 requestRouter.post('/', createRequestController);
+requestRouter.get('/pending/to/:id', getPendingRequestsForUserId);
 
 export default requestRouter;
