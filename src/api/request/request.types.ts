5d70cbd2b340880b830aa11a6bbf3b306fdd5596
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-06-06 21:17:18 -0400

    Add Request type definition

--- src/api/request/request.types.ts
+++ src/api/request/request.types.ts
@@ -2,14 +2,28 @@ import {
   String as RtString,
   Number as RtNumber,
   Record as RtRecord,
+  Union as RtUnion,
+  InstanceOf as RtInstanceOf,
 } from 'runtypes';
 
 export const CreateRequestBody = RtRecord({
   type: RtString,
-  toUser: RtNumber,
-  fromUser: RtNumber,
+  toUser: RtString,
+  fromUserId: RtNumber,
 });
 
 export const GetPendingRequestsParam = RtRecord({
   id: RtString,
+});
+
+export const Request = RtRecord({
+  id: RtNumber,
+  type: RtString,
+  dateRequested: RtUnion(
+    RtInstanceOf(Date),
+    RtString.withConstraint((x: string) => (x && x !== null && typeof x === 'string' && !Number.isNaN(Date.parse(x)))),
+  ),
+  fromUser: RtNumber,
+  toUser: RtNumber,
+  status: RtString,
 });
