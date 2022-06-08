33d92b49e8351647771a1874cebf98eff61e2e0a
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-06-07 22:36:41 -0400

    Add route to user details controller

--- src/api/user/user.routes.ts
+++ src/api/user/user.routes.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import userControllers from './user.controllers';
 
-const { createUserController } = userControllers;
+const { createUserController, getUserDetailsController } = userControllers;
 
 const usersRouter = express.Router();
 
 usersRouter.post('/', createUserController);
+usersRouter.get('/details/:id', getUserDetailsController);
 
 export default usersRouter;
