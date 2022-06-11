b37b684b6bf55a2e96bb35484ce4553d459e869c
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-06-10 23:19:11 -0400

    Add updateRequestController to request routes

--- src/api/request/request.routes.ts
+++ src/api/request/request.routes.ts
@@ -1,11 +1,16 @@
 import express from 'express';
 import requestControllers from './request.controllers';
 
-const { createRequestController, getPendingRequestsForUserId } = requestControllers;
+const {
+  createRequestController,
+  getPendingRequestsForUserId,
+  updateRequestController,
+} = requestControllers;
 
 const requestRouter = express.Router();
 
 requestRouter.post('/', createRequestController);
 requestRouter.get('/pending/to/:id', getPendingRequestsForUserId);
+requestRouter.put('/:id', updateRequestController);
 
 export default requestRouter;
