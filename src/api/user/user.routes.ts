08c117e216428df17c7d513cfaa3403864b6de4d
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-06-18 00:15:27 -0400

    Add getContactsController to user routes

--- src/api/user/user.routes.ts
+++ src/api/user/user.routes.ts
@@ -5,12 +5,14 @@ const {
   createUserController,
   getUserDetailsController,
   addContactController,
+  getContactsController,
 } = userControllers;
 
 const usersRouter = express.Router();
 
 usersRouter.post('/', createUserController);
 usersRouter.get('/details/:id', getUserDetailsController);
+usersRouter.get('/:id/contacts', getContactsController);
 usersRouter.put('/:id/contacts', addContactController);
 
 export default usersRouter;
