80b6ebd93419a0fff7acb5d2d892da155c8f4762
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-06-20 01:35:40 -0400

    Reimplement controller for getting messages

--- src/api/message/message.controllers.ts
+++ src/api/message/message.controllers.ts
@@ -1,13 +1,40 @@
 import { Request, Response, NextFunction } from 'express';
-import { MessageArray, RoomIdRequestParam } from './message.types';
+import { MessageArray, SenderRecipientBody } from './message.types';
 import Message from './message.model';
 
-const getMessagesByRoomIdController = async (req: Request, res: Response, next: NextFunction) => {
+const getMessagesBySenderRecipientController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
   try {
-    const { roomId } = RoomIdRequestParam.check({ id: req.params['id'] });
-    const messages = MessageArray.check(await Message.findAll({ where: { roomId } }));
-    res.status(200).json(messages);
+    const { senderUsername, recipientUsername } = SenderRecipientBody.check(
+      req.body,
+    );
+
+    // Get messages between two users
+    const senderMessages = MessageArray.check(
+      await Message.findAll({ where: { senderUsername, recipientUsername } }),
+    );
+
+    const recipientMessages = MessageArray.check(
+      await Message.findAll({
+        where: {
+          senderUsername: recipientUsername,
+          recipientUsername: senderUsername,
+        },
+      }),
+    );
+
+    // Combine messages into a single array of all messages between users
+    const allMessages = senderMessages.concat(recipientMessages).sort();
+
+    res.status(200).json(allMessages);
   } catch (error: unknown) {
     next(error);
   }
+};
+
+export default {
+  getMessagesBySenderRecipientController,
 };
