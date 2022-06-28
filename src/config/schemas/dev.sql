f7f7bfb0b64f8f2c53383e856e61362c95c2b53f
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-06-27 21:14:28 -0400

    Add unique constraint to username field

--- src/config/schemas/dev.sql
+++ src/config/schemas/dev.sql
@@ -1,7 +1,7 @@
 -- Setup schema
 CREATE TABLE users (
   id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  username TEXT,
+  username TEXT UNIQUE,
   name TEXT,
   password_hash TEXT,
   date_registered TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
