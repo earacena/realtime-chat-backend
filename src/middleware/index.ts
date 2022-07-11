8ce7abbcb7b002b0c912e87dbfdbea1096a8d44d
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-07-11 16:24:21 -0400

    Shorten 'authenticate' exported name to 'auth'

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,2 +1,2 @@
-export { default as authenticate } from './authenticate.middleware';
+export { default as auth } from './authenticate.middleware';
 export { default as errorHandler } from './errorHandler.middleware';
