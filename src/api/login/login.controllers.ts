4d542ac0e704070a093fd472d0f37d1e14e2e947
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2022-07-11 16:00:52 -0400

    Format file

--- src/api/login/login.controllers.ts
+++ src/api/login/login.controllers.ts
@@ -7,15 +7,16 @@ import { User as UserType } from '../user/user.types';
 import { SECRET_JWT_KEY } from '../../config';
 import LoginRequest from './login.types';
 
-const loginController = async (req: Request, res: Response, next: NextFunction) => {
+const loginController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
   try {
     const { username, password } = LoginRequest.check(req.body);
     const user = UserType.check(await User.findOne({ where: { username } }));
 
-    const isPasswordCorrect = await bcrypt.compare(
-      password,
-      user.passwordHash,
-    );
+    const isPasswordCorrect = await bcrypt.compare(password, user.passwordHash);
 
     if (!isPasswordCorrect) {
       res.status(400).json({
@@ -32,14 +33,12 @@ const loginController = async (req: Request, res: Response, next: NextFunction) 
 
     const token = RtString.check(JwtSign(userDetails, SECRET_JWT_KEY));
 
-    res
-      .status(200)
-      .send({
-        token,
-        id: user.id,
-        username: user.username,
-        name: user.name,
-      });
+    res.status(200).send({
+      token,
+      id: user.id,
+      username: user.username,
+      name: user.name,
+    });
     return;
   } catch (error: unknown) {
     next(error);
